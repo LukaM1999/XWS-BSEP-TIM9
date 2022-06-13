621b5f0d33023579530251d2807b8e49f8f4ccda
KisicM <kisic.ra183.2018@uns.ac.rs>
2022-06-13 23:17:36 +0200

    feat: https on pki

---
+++ bsep/src/main/java/com/bsep/bsep/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.bsep.bsep.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize ->
+                        authorize.anyRequest().permitAll())
+                .build();
+    }
+}
--- bsepFrontend/bsep/vue.config.js
+++ bsepFrontend/bsep/vue.config.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+
+
 module.exports = {
     configureWebpack: {
         devtool: 'source-map',
@@ -10,6 +13,11 @@ module.exports = {
 
     },
     devServer: {
-        proxy: "http://localhost:9000"
+        host: 'localhost',
+        https: {
+            key: fs.readFileSync('../../cert/server-key.pem'),
+            cert: fs.readFileSync('../../cert/server-cert.pem'),
+        },
+        //public: process.env.VUE_APP_BACKEND,
     },
 }
